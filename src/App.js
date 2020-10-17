00914b5a6d68c1021353aa27402916428db3940b
Joao Olenscki <joao.olenscki@usp.br>
2020-10-17 13:57:44 -0300

    primeira calculadora

--- src/App.js
+++ src/App.js
@@ -1,12 +1,96 @@
 import React from 'react';
 import './App.css';
 
+
 export default class App extends React.Component {
   state = {
       currentOperand : '',
       previousOperand : '',
       operation : ''
   };
+
+  funcAdd(number){ //funcao para concatenar digitos
+    if (number === "."){ //se estamos adicionando um separador decimal
+      if (this.state.currentOperand === ''){ //mas nao temos numero ainda
+        this.setState(state => ({
+        currentOperand : '0.1' //valor atual é 0.1
+      }));
+    } else if(this.state.currentOperand.includes('.')){ //se temos um numero e ele ja tem
+                                                        // um sep decimal, passe
+    } else{ //se ainda nao tem separador decimal, adicionar
+      this.setState(state => ({
+        currentOperand : this.state.currentOperand + number
+      }));
+    }    
+  } else{ //senao (nao é '.'), apenas concatenar
+    this.setState(state => ({
+      currentOperand : this.state.currentOperand + number
+    }));
+  }
+  }
+
+  funcAddOperand(op_name){ //funcao para adicionar um operador
+    if (this.state.previousOperand !== ''){ 
+      this.funcEqual()
+      this.setState(state => ({
+        operation : op_name, //seta o operador
+      }));
+    } else{
+      this.setState(state => ({
+        previousOperand: this.state.currentOperand, //valor atual vira valor anterior
+        operation : op_name, //seta o operador
+        currentOperand: '' //limpa valor atual
+      }));
+    } 
+  }
+
+funcEqual(){
+  const cOFloat = parseFloat(this.state.currentOperand);
+  const pOFloat = parseFloat(this.state.previousOperand);
+  var result;
+
+  switch(this.state.operation){
+    case "+":
+      result = cOFloat + pOFloat;
+    break;
+
+    case "-":
+      result = cOFloat - pOFloat;
+    break;
+      
+    case "x":
+      result = cOFloat * pOFloat;
+    break;
+
+    case "÷":
+      result = cOFloat / pOFloat;
+    break;
+
+    default:
+      result = cOFloat;
+    }
+
+  if (!isFinite(result)) {
+    if (isNaN(result)){
+      result = "Error 404: Number not found!";
+    } else {
+      result = "Por zero não!"
+    }
+  }
+
+this.setState(state => ({
+        previousOperand: result,
+        operation : '', 
+        currentOperand: '' 
+      }));
+}
+
+funcClear(){
+  this.setState(state => ({
+    currentOperand: '' 
+  }));
+
+}
  
 render() {
   return (
@@ -15,27 +99,27 @@ render() {
         <div className="container">
               <div className="calculator">
                 <div className="calculator_display">
-                  <div className="calculator_display_last">0</div>
-                  <div className="calculator_display_current">0</div>
+                  <div className="calculator_display_last">{this.state.previousOperand + this.state.operation}</div>
+                  <div className="calculator_display_current">{this.state.currentOperand}</div>
                 </div>
                 <div className="calculator_keys">
-                  <button className="key-operator" data-ops="plus">+</button>
-                  <button className="key-operator" data-ops="minus">-</button>
-                  <button className="key-operator" data-ops="times">x</button>
-                  <button className="key-operator" data-ops="divided by">÷</button>
-                  <button className="number" data-num="7">7</button>
-                  <button className="number" data-num="8">8</button>
-                  <button className="number" data-num="9">9</button>
-                  <button className="number" data-num="4">4</button>
-                  <button className="number" data-num="5">5</button>
-                  <button className="number" data-num="6">6</button>
-                  <button className="number" data-num="1">1</button>
-                  <button className="number" data-num="2">2</button>
-                  <button className="number" data-num="3">3</button>
-                  <button className="number" data-num="0">0</button>
-                  <button className="number" >.</button>
-                  <button id = "clear" className="all-clear" >AC</button>
-                  <button className="equal-sign" data-result="">=</button>
+                  <button className="key-operator" data-ops="plus" onClick={() => this.funcAddOperand('+')}>+</button>
+                  <button className="key-operator" data-ops="minus" onClick={() => this.funcAddOperand('-')}>-</button>
+                  <button className="key-operator" data-ops="times" onClick={() => this.funcAddOperand('x')}>x</button>
+                  <button className="key-operator" data-ops="divided by" onClick={() => this.funcAddOperand('÷')}>÷</button>
+                  <button className="number" data-num="7" onClick={() => this.funcAdd(7)}>7</button>
+                  <button className="number" data-num="8" onClick={() => this.funcAdd(8)}>8</button>
+                  <button className="number" data-num="9" onClick={() => this.funcAdd(9)}>9</button>
+                  <button className="number" data-num="4" onClick={() => this.funcAdd(4)}>4</button>
+                  <button className="number" data-num="5" onClick={() => this.funcAdd(5)}>5</button>
+                  <button className="number" data-num="6" onClick={() => this.funcAdd(6)}>6</button>
+                  <button className="number" data-num="1" onClick={() => this.funcAdd(1)}>1</button>
+                  <button className="number" data-num="2" onClick={() => this.funcAdd(2)}>2</button>
+                  <button className="number" data-num="3" onClick={() => this.funcAdd(3)}>3</button>
+                  <button className="number" data-num="0" onClick={() => this.funcAdd(0)}>0</button>
+                  <button className="number" data-num="." onClick={() => this.funcAdd('.')}>.</button>
+                  <button id = "clear" className="all-clear" onClick={() => this.funcClear()}>AC</button>
+                  <button className="equal-sign" data-result="" onClick={() => this.funcEqual()}>=</button>
                 </div>
               </div>
           </div>
@@ -43,228 +127,4 @@ render() {
     </div>
   );
 }
-
-
-
-function(){
-  var el = function(element){
-    if (element.charAt(0) === "#"){
-      return document.querySelector(element);
-    }
-    return document.querySelectorAll(element);
-  };
-
-  var dpreviousOperand = el(".calculator_display_last"),
-      dcurrentOperand = el(".calculator_display_current"),
-      dequal = el(".equal-sign"),
-      dnumbers = el(".number"),
-      doperations = el(".key-operator"),
-      previousOperand = this.previousOperand,
-      currentOperand = this.currentOperand,
-      operation = this.operation,
-      resultNum;
-
-var setNum = function(){
-  if (resultNum){
-    currentOperand = this.getAttribute("data-num");
-    resultNum = '';
-  } else {
-    currentOperand += this.getAttribute("data-num");
-  }
-
-  dcurrentOperand.innerHTML = currentOperand
-};
-
-var moveNum = function(){
-  previousOperand = currentOperand;
-  currentOperand = '';
-  operation = this.getAttribute("data-ops");
-  dpreviousOperand.innerHTML = previousOperand;
-
-  dequal.setAttribute("data-result", "");
-};
-
-var displayNum = function(){
-  currentOperand = parseFloat(currentOperand);
-  previousOperand = parseFloat(previousOperand);
-
-  switch(operation){
-    case "plus":
-      resultNum = currentOperand + previousOperand;
-      break;
-
-      case "minus":
-        resultNum = currentOperand - previousOperand;
-      break;
-        
-      case "times":
-        resultNum = currentOperand * previousOperand;
-        break;
-
-      case "divided by":
-        resultNum = currentOperand / previousOperand;
-        break;
-
-      default:
-        resultNum = currentOperand;
-    }
-
-  if (!isFinite(resultNum)) {
-    if (isNaN(resultNum)){
-      resultNum = "Error 404: Number not found!";
-    } else {
-      resultNum = "Por zero não!"
-    }
-  }
-
-  dcurrentOperand.innerHTML = resultNum;
-  dpreviousOperand.innerHTML = '';
-
-  currentOperand = 0
-  previousOperand = resultNum;
-};
-
-
-var clearAll = function(){
-  currentOperand = '';
-  dcurrentOperand.innerHTML = '0';
-  dequal.setAttribute("data-result", resultNum);
-};
-
-
-for (var i = 0, l = dnumbers.length; i < l; i++){
-  dnumbers[i].onclick = setNum;
 }
-
-for (var i = 0, l = doperations.length; i < l; i++){
-  doperations[i].onclick = moveNum;
-}
-
-dequal.onclick = displayNum;
-
-el("#clear").onclick = clearAll;
-
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function isNumber(item) {
-//   return /[0-9]+/.test(item); 
-// }
-
-// function operate(numberOne, numberTwo, operation) {
-//   const one = Big(numberOne || "0");
-//   const two = Big(numberTwo || (operation === "÷" || operation === 'x' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
-//   if (operation === "+") {
-//     return one.plus(two).toString();
-//   }
-//   if (operation === "-") {
-//     return one.minus(two).toString();
-//   }
-//   if (operation === "x") {
-//     return one.times(two).toString();
-//   }
-//   if (operation === "÷") {
-//     if (two === "0") {
-//       alert("Divide by 0 error");
-//       return "0";
-//     } else {
-//       return one.div(two).toString();
-//     }
-//   }
-//   throw Error(`Unknown operation '${operation}'`);
-// }  
-
-// function calculate(state, buttonName) {
-//   if (buttonName === "AC") {
-//     return {
-//       currentOperand : '',
-//       operation : ''
-//   };
-//   }
-
-//   if (isNumber(buttonName)){
-//     if (buttonName === "0" && state.currentOperand === "0"){
-//       return {};
-//     }
-//     if (state.operation){
-//       if (state.currentOperand){
-//         return {currentOperand : state.currentOperand + buttonName}
-//       }
-//       return {currentOperand : buttonName}
-//     } 
-
-//   if (state.currentOperand) {
-//     const next = state.currentOperand === "0" ? buttonName : state.currentOperand + buttonName;
-//     return {
-//       currentOperand : next
-//     };
-//   }
-//   return {
-//     currentOperand : buttonName
-//   };
-// }
-
-// if (buttonName === ".") {
-//   if (state.currentOperand) {
-//     // ignore a . if the next number already has one
-//     if (state.currentOperand.includes(".")) {
-//       return {};
-//     }
-//     return { currentOperand: state.currentOperand + "." };
-//   }
-//   return { currentOperand: "0." };
-// }
-
-// if (buttonName === "=") {
-//   if (state.currentOperand && state.operation) {
-//     return {
-//       previousOperand: operate(state.previousOperand, state.currentOperand, state.operation),
-//       currentOperand: '',
-//       operation: '',
-//     };
-//   } else {
-//     return {};
-//   }
-// }
-
-
-
-// if (state.operation) {
-//   return {
-//     previousOperand: operate(state.previousOperand, state.currentOperand, state.operation),
-//     currentOperand: '',
-//     operation: buttonName,
-//   };
-// }
-
-
-// if (!state.currentOperand) {
-//   return { operation: buttonName };
-// }
-
-// return {
-//   previousOperand: state.currentOperand,
-//   currentOperand: '',
-//   operation: buttonName,
-// };
-//}
-
-// const integerDisplay = parseFloat(integerDigits);toLocaleString('pt-BR'), {maximumFractionDigits: 0});
-
